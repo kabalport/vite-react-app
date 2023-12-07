d3a50b6a744feb98af0d3fb8625af6d42b512fa4
최대열 <dychoi@brainednet.com>
2023-12-07 15:05:36 +0900

    header develop

--- src/layouts/MainLayout/MainHeader.tsx
+++ src/layouts/MainLayout/MainHeader.tsx
@@ -1,21 +1,79 @@
 import * as React from 'react';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+import Link from "@mui/material/Link";
+import MenuIcon from '@mui/icons-material/Menu';
+import IconButton from '@mui/material/IconButton';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import {NavLink} from "react-router-dom";
 
 function MainHeader() {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+    const [drawerOpen, setDrawerOpen] = React.useState(false);
+
+    const toggleDrawer = (open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+        setDrawerOpen(open);
+    };
+
+    const navLinks = ['About', 'Services'];
+
+    const mobileMenu = (
+        <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
+            <List>
+                {navLinks.map((text) => (
+                    <ListItem key={text} onClick={toggleDrawer(false)}>
+                        <NavLink to={`/${text.toLowerCase()}`} activeClassName="activeLink">
+                            <ListItemText primary={text} />
+                        </NavLink>
+                    </ListItem>
+                ))}
+            </List>
+        </Drawer>
+    );
+
     return (
         <React.Fragment>
-            <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                <Typography
-                    component="h2"
-                    variant="h5"
-                    color="inherit"
-                    align="center"
-                    noWrap
-                    sx={{ flex: 1 }}
-                >
-                    타로메이트
-                </Typography>
+            <Toolbar sx={{
+                borderBottom: 1,
+                borderColor: 'divider',
+                backgroundColor: '#f0f0f0',
+                boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2)',
+                padding: '10px',
+                justifyContent: 'space-between'
+            }}>
+
+                <Link color="inherit" href="/">
+                    <img
+                        src="/images/TarotMate_logo.png"
+                        alt="TarotMate Logo"
+                        style={{ height: '50px' }}
+                    />
+                </Link>
+
+                {isMobile ? (
+                    <IconButton color="inherit" onClick={toggleDrawer(true)}>
+                        <MenuIcon />
+                    </IconButton>
+                ) : (
+                    <div>
+                        {navLinks.map((text) => (
+                            <Link color="inherit" href={`/${text.toLowerCase()}`} key={text}>
+                            <Button color="inherit">{text}</Button>
+                            </Link>
+                        ))}
+                    </div>
+                )}
+
+                {isMobile && mobileMenu}
             </Toolbar>
         </React.Fragment>
     );
