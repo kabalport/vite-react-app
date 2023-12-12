f7ff0b9c9d5ab224c25130c90e5a02e037483a7f
최대열 <dychoi@brainednet.com>
2023-12-12 20:35:14 +0900

    refactoring22

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,7 +1,10 @@
 export default function Button({color, children}:any) {
-
+    const onClick: any = () => {
+        alert("버튼을 클릭했습니다.");
+    };
     return (
         <button
+            onClick={onClick}
             style={{
                 backgroundColor: color
             }}>
