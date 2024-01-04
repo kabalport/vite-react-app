0841c34ef95375df5cfa8259346c5b399bd18c53
dychoi <dychoi@brainednet.com>
2024-01-04 19:25:32 +0900

    naras porject init2

--- src/pages/NarasApp/NarasApp.tsx
+++ src/pages/NarasApp/NarasApp.tsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
+import { fetchCountries } from "./api/api.ts";
+
 export default function NarasApp() {
-    return (
-        <div>Home
-    </div>
-    );
+    const [countries, setCountries] = useState([]);
+
+    const setInitData = async () => {
+        const data = await fetchCountries();
+        setCountries(data);
+    };
+
+    useEffect(() => {
+        setInitData();
+    }, []);
+
+    return <div>Home</div>;
 }
